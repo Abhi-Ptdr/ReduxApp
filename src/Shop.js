ea0bc1e04e93a9b6fbcdfd2bb7567e37f8d318a2
Abhishek Patidar <abhipatidar253@gmail.com>
2025-01-30 00:27:42 +0530

    Updating state from a different component v78

--- src/Shop.js
+++ src/Shop.js
@@ -1,12 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
 
 function Shop() {
+  const dispatch = useDispatch();
+  const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch);
+  //destructuring the actions
+
+  const balance = useSelector(state => state.amount)
+
   return (
     <div>
         <h2>Deposit/Withdraw Money</h2>
-        <button className="btn btn-primary mx-2">-</button>
+        {/* 
+        <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.withdrawMoney(100))}}>-</button>
           Update Balance
-        <button className="btn btn-primary mx-2">+</button>
+        <button className="btn btn-primary mx-2" onClick={() => {dispatch(actionCreators.depositMoney(100))}}>+</button> 
+        this is how we can do without using bindActionCreators but we can use bindActionCreators to make it more readable.
+        */}
+
+        <button className="btn btn-primary mx-2" onClick={() => {withdrawMoney(100)}}>-</button>
+          Update Balance ({balance})
+        <button className="btn btn-primary mx-2" onClick={() => {depositMoney(100)}}>+</button>
+    
     </div>
   )
 }
